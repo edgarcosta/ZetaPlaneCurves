1fa32975458bdf1084831637d3e4fd8d54d660a2
Edgar Costa <edgarc@mit.edu>
2020-10-28 00:01:38 -0400

    rewriting NormalizationCorrections and other improvements

--- ZetaPlaneCurves.m
+++ ZetaPlaneCurves.m
@@ -15,107 +15,47 @@
   Edgar Costa, 2020
 ***********************************************************************************************/
 
-
+declare verbose ZetaPlaneCurve, 2;
 // Given f defining a plane curve in P2
 // computes the diferences of point counts between the plane model and its normalisation for GF(p^r)
 // at the moment we only support nodal singularities
 function NormalizationCorrections(f, up_to_r)
+  corrections := [0 : _ in [1..up_to_r]];
   R3 := Parent(f);
   assert Rank(R3) eq 3;
   assert IsPrimeField(BaseRing(R3));
   p := Characteristic(BaseRing(R3));
-  //C := Curve(ProjectiveSpace(R3), f);
-  //assert IsNodalCurve(C);
-  R2<x,y> := PolynomialRing(BaseRing(R3), 2);
-
-  proj2 := hom< R3 -> R2 | [x, y, 1]>;
-  F := proj2(f);
-  Fx := Derivative(F, x);
-  Fy := Derivative(F, y);
-  Re1 := Resultant(Fx, Fy, y);
-  Re2 := Resultant(Fx, F, y);
-  Re3 := Resultant(Fy, F, y);
-  R1<x1> := PolynomialRing(BaseRing(R3));
-  proj1 := hom< R2 -> R1 | [x1, 1]>;
-  H := GCD([proj1(elt) : elt in [Re1, Re2, Re3]]);
-
-  singular_points := [* *];
-  if H ne 1 then
-    // the curve is not smooth in the affine coordinate z = 1
-    for fac in Factorisation(H) do
-      pol, mul := Explode(fac);
-      // I only wrote the code to handle nodes at the moment
-      assert mul le 2;
-      deg := Degree(pol);
-      K<a> := GF(p^deg);
-      Ku<u> := PolynomialRing(K);
-      K2<lx,ly> := PolynomialRing(K, 2);
-      for xroot in Roots(Evaluate(pol, u)) do
-        Px, Pxmul := Explode(xroot);
-        assert Pxmul eq 1;
-        // determine the Y coordinate of the singularity
-        possiblePy := Roots(Evaluate(F, [Px, u]));
-        for yroot in possiblePy do
-          Py, Pymul := Explode(yroot);
-          if Pymul gt 1 then
-            FatP := Evaluate(F, [lx+ Px, ly + Py]);
-            if MonomialCoefficient(FatP, lx) eq 0 and MonomialCoefficient(FatP, ly) eq 0 then
-              FatPdegree2 := MonomialCoefficient(FatP, lx^2) * lx^2;
-              FatPdegree2 +:= MonomialCoefficient(FatP, ly^2) * ly^2;
-              FatPdegree2 +:= MonomialCoefficient(FatP, lx * ly) * lx * ly;
-              normal_deg := (3 - #Factorisation(FatPdegree2)) * deg;
-              Append(~singular_points, <[Px, Py, 1], deg, normal_deg>);
-            end if;
-          end if;
-        end for;
-      end for;
-    end for;
+  C := Curve(ProjectiveSpace(R3), f);
+  assert IsNodalCurve(C); // Is possible to generalize to other types of singularities
+  if not IsSingular(C) then
+    return corrections;
   end if;
-  // now deal with singularities at infinity
-  // x := 1
-  // z := 0
-  deg := 1;
-  K<a> := GF(p^deg);
-  Ku<u> := PolynomialRing(K);
-  K2<lx, ly> := PolynomialRing(K, 2); // lx will play the role of lz
-  possiblePy := Roots(Evaluate(f, [1, u, 0]));
-  // a bit repetitive code, but too lazy do anything different
-  for yroot in possiblePy do
-    Py, Pymul := Explode(yroot);
-    if Pymul gt 1 then
-      FatP := Evaluate(f, [1, ly + Py, lx + 0]);
-      if MonomialCoefficient(FatP, lx) eq 0 and MonomialCoefficient(FatP, ly) eq 0 then
-        FatPdegree2 := MonomialCoefficient(FatP, lx^2) * lx^2;
-        FatPdegree2 +:= MonomialCoefficient(FatP, ly^2) * ly^2;
-        FatPdegree2 +:= MonomialCoefficient(FatP, lx * ly) * lx * ly;
-        normal_deg := (3 - #Factorisation(FatPdegree2)) * deg;
-        Append(~singular_points, <[1, Py, 0], deg, normal_deg>);
+
+  // one could work over the algebraic closure, but that sometime fails
+  for m in [1..up_to_r] do
+    Fm := GF(p^m);
+    Cm := ChangeRing(C, Fm);
+    for pt in SingularPoints(Cm) do
+      deg := LCM([Degree(MinimalPolynomial(elt)) : elt in Coordinates(pt)]);
+      if deg eq m then
+        eqTC := DefiningPolynomials(TangentCone(pt));
+        assert #eqTC eq 1;
+        // the degree of the extension where the 2 points will arise
+        // We care about how it factors over the field of definition of the singular pt
+        ndeg := (3 - #Factorisation(ChangeRing(eqTC[1], GF(p^deg)))) * deg;
+        // replace the counts of singular points by the counts of their normalisations
+        extension := deg;
+        while extension le up_to_r do
+          corrections[extension] -:= 1;
+          extension +:= deg;
+        end while;
+        extension := ndeg;
+        while extension le up_to_r do
+          corrections[extension] +:= 2;
+          extension +:= ndeg;
+        end while;
       end if;
-    end if;
-  end for;
-  // for last the point (0,1,0)
-  FatP := Evaluate(f, [lx, 1, ly]);
-  if MonomialCoefficient(FatP, lx) eq 0 and MonomialCoefficient(FatP, ly) eq 0 then
-    FatPdegree2 := MonomialCoefficient(FatP, lx^2) * lx^2;
-    FatPdegree2 +:= MonomialCoefficient(FatP, ly^2) * ly^2;
-    FatPdegree2 +:= MonomialCoefficient(FatP, lx * ly) * lx * ly;
-    normal_deg := (3 - #Factorisation(FatPdegree2)) * deg;
-    Append(~singular_points, <[0, 1, 0], deg, normal_deg>);
-  end if;
-  // now compute the corrections
-  corrections := [0 : _ in [1..up_to_r]];
-  for pt in singular_points do
-    _, deg, ndeg := Explode(pt);
-    extension := deg;
-    while extension le up_to_r do
-      corrections[extension] -:= 1;
-      extension +:= deg;
-    end while;
-    extension := ndeg;
-    while extension le up_to_r do
-      corrections[extension ] +:= 2;
-      extension +:= ndeg;
-    end while;
+    end for;
   end for;
   return corrections;
 end function;
@@ -144,21 +84,26 @@ end function;
 // Compute the matrices A_{F^s} = M_s mod p^e (a=1) for s in [0..e], via Lemma 3.2 using brute force
 // The dimension of the returned matrices is Binomial(d*s+2,2), where d=Degree(f)
 function mats(f, p, e)
+    t0 := Cputime();
     R1 := PolynomialRing(Integers(p^e));
     R2 := PolynomialRing(R1);
     F := Evaluate(ChangeRing(ChangeRing(f,Integers()),Integers(p^e)), [R1.1, R2.1, 1]);
     // compute all the powers of f necessary
     F := F^(p-1);
-    fp1s := [1, F, F^2];
-    for s in [3..e] do
-        Append(~fp1s, F*fp1s[s]);
+    fp1s := [1, F];
+    for s in [#fp1s..e] do
+      // trying to keep the mults balanced
+      k1 := s div 2;
+      k2 := s - k1;
+      Append(~fp1s, fp1s[k1 + 1]*fp1s[k2 + 1]);
     end for;
     assert #fp1s eq e + 1;
+
 
     // list of exponent vectors of monomial basis for R[x,y,z]_(d*s)
     R := PolynomialRing(Integers(),3);
     d := Degree(f);
-    Bds := [[Vector([Degree(m,R.i):i in [1..3]]):m in MonomialsOfDegree(R,d*s)] : s in [0..e]];
+    Bds := [[[Degree(m,R.i) : i in [1..3]]: m in MonomialsOfDegree(R,d*s)] : s in [0..e]];
 
 
     // for g := f^((p-1)s) return M_s
@@ -169,7 +114,7 @@ function mats(f, p, e)
                     (Min(ee) lt 0) or (ee[1]+1 gt #cc) or (ee[2] + 1 gt #cc[ee[1]+1])
                           select 0 else cc[ee[1]+1][ee[2]+1]
                           where ee is [p * u[k] - v[k] : k in [1..3]]
-                      : u in B] : v in B]);
+                      : u in B] : v in B]) where cc := [Coefficients(a) : a in Coefficients(g)];
         return M;
     end function;
     return [*mat(fp1s[s+1], s, Bds[s+1], p) : s in [0..e] *];
@@ -181,14 +126,18 @@ end function;
 // Compute #X_f(F_p^r) mod p^e, where X_f is the plane curve f(x,y,z) = 0 using the trace formula (Theorem 3.1)
 // see ApproximateNumberOfPoints for possible improvments
 function points_trace_formula(f, p, r, e : Ms:=[])
-  assert p gt 1 + e/r;
+  assert p ge 1 + e/r;
   if Ms eq [* *] then
       Ms := mats(f, p, e);
   end if;
-  R := Integers(p^e);
-  N := R!MissingPoints(ChangeRing(f,GF(p^r))) +
-         (R!p^r-R!1)^2*&+[(-1)^s*R!Binomial(e,s)*Trace(Ms[s+1]^r):s in [0..e]];
-  return N;
+
+  missing := MissingPoints(ChangeRing(f,GF(p^r)));
+  vprint ZetaPlaneCurve, 1: "r = ", r;
+  vprint ZetaPlaneCurve, 1: "MissingPoints = ", missing;
+  formula := (p^r-1)^2*&+[(-1)^s*Binomial(e,s)*Trace(Ms[s+1]^r):s in [0..e]];
+  vprint ZetaPlaneCurve, 1: "Trace sum = ", formula;
+  vprint ZetaPlaneCurve, 1: "total = ", missing + formula;
+  return missing + formula;
 end function;
 
 // Computes the zeta function of f(x,y,z) = 0 over Fp
@@ -204,24 +153,32 @@ intrinsic LPolynomial(f :: RngMPolElt: KnownFactor := false, corrections := fals
   require not IsDivisibleBy(Degree(f), p) : "p divides the degree of f"; // possible to work around
   C := Curve(ProjectiveSpace(R3), f);
   g := Genus(Curve(ProjectiveSpace(Parent(f)), f));
- if KnownFactor cmpeq false then KnownFactor := PolynomialRing(Integers())!1; end if;
-  up_to_r := Ceiling((2*g - Degree(KnownFactor))/2);
+  if KnownFactor cmpeq false then KnownFactor := PolynomialRing(Integers())!1; end if;
+  up_to_r := (2*g - Degree(KnownFactor)) div 2;
+  vprint ZetaPlaneCurve, 1: "Using up_to_r = ", up_to_r;
   if corrections cmpeq false then
     require IsNodalCurve(C) : "the plane curve C does not have  only nodes as singularities, please provide point counts corrections for the normalisation";
     corrections := NormalizationCorrections(ChangeRing(f, GF(p)), g);
   else
     require #corrections ge up_to_r : "need corrections up to GF(p^" cat IntegerToString(up_to_r) cat ")";
   end if;
+  vprint ZetaPlaneCurve, 1: "corrections = ", corrections;
   // one could decrease 2*g to 2*up_to_r by correcting the point counts with known factor and just computing the unkown factor
-  e := Ceiling(Log(p,2*g*p^(up_to_r/2)));
+  e := Ceiling(Log(p,4*up_to_r*p^(up_to_r/2)));
+  vprint ZetaPlaneCurve, 1: "Using prec = ", e;
   require p gt 1 : "p is too small"; // only implemented the simpler version of the trace formula
   // Compute M_s for s in [0..e]
   Ms := mats(f, p, e);
-  tmodpe := [p^r+1-Integers()!points_trace_formula(f, p, r, e : Ms:=Ms) - corrections[r]
+  KnownFrob := KnownFactor eq 1 select Matrix([[0]]) else CompanionMatrix(Reverse(KnownFactor));
+  tmodpe := [
+    p^r + 1
+    -Integers()!points_trace_formula(f, p, r, e : Ms:=Ms) 
+    - corrections[r]
+    - Trace(KnownFrob^r)
     : r in [1..up_to_r]];
   lift := func<m, n| 2*a gt n select a-n else a where a:= (m mod n)>;
   t := [lift(elt, p^e) : elt in tmodpe];
-  return TracesToLPolynomial(t, p, g: KnownFactor:=KnownFactor);
+  return TracesToLPolynomial(t, p, up_to_r) * KnownFactor;
 end intrinsic;
 
 
@@ -229,4 +186,8 @@ intrinsic LPolynomial(f::RngMPolElt, p::RngIntElt : KnownFactor:=false, correcti
 {The L-polynomial of the of the projective normalisation of the curve C defined by the zero locus of f in P^3_GF(p), where C has at most nodal singularities and f in Q[x,y,z].}
  return  LPolynomial(PolynomialRing(GF(p),3)!f: KnownFactor := KnownFactor, corrections := false);
 end intrinsic;
+
+
+
+
 
